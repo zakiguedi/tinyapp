38cfce4ac26c292812606b2198854e1face7f9e7
zakiguedi <zaki.guedi@gmail.com>
2023-02-20 19:15:14 -0500

    template engine

--- express_server.js
+++ express_server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
